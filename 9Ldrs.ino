58bdc78b38c057f1b8235d50a62d7f026dce117a
Mrmk2002 <115863877+Mrmk2002@users.noreply.github.com>
2024-05-29 22:43:57 +0300

    Update 9Ldrs.ino

--- 9Ldrs.ino
+++ 9Ldrs.ino
@@ -1,90 +1,77 @@
 #include <Servo.h>
 
 Servo horizontal; // horizontal servo
-int servoh = 180;
+Servo vertical;   // vertical servo
+
+int servoh = 90;
 int servohLimitHigh = 175;
 int servohLimitLow = 5;
 
-Servo vertical; // vertical servo
-int servov = 45;
+int servov = 90;
 int servovLimitHigh = 100;
 int servovLimitLow = 1;
 
-// LDR pin connections
-int ldrtl = A0; // LDR top left - BOTTOM LEFT
-int ldrrt = A3; // LDR top right - BOTTOM RIGHT
-int ldrml = A1; // LDR mid left - TOP LEFT
-int ldrrl = A2; // LDR mid right - TOP RIGHT
-int ldrbl = A4; // LDR bottom left
-int ldrbm = A5; // LDR bottom mid
-int ldrtm = A6; // LDR top mid
-int ldrtr = A7; // LDR top right
-int ldrc = A8;  // LDR center - CENTER
+// LDR pin connections with classifications
+int ldrTopLeft = A1;      // Top left
+int ldrTopMid = A6;       // Top mid
+int ldrTopRight = A2;     // Top right
+int ldrMidLeft = A4;      // Mid left
+int ldrCenter = A8;       // Center (Mid-Mid)
+int ldrMidRight = A7;     // Mid right
+int ldrBottomLeft = A0;   // Bottom left
+int ldrBottomMid = A5;    // Bottom mid
+int ldrBottomRight = A3;  // Bottom right
 
 void setup() {
   horizontal.attach(9);
   vertical.attach(10);
-  horizontal.write(180);
-  vertical.write(45);
-  delay(2500);
+  horizontal.write(servoh);
+  vertical.write(servov);
+  delay(2500); // Allow servos to reach initial position
 }
 
 void loop() {
-  int lt = analogRead(ldrtl); // LDR top left - BOTTOM LEFT
-  int rt = analogRead(ldrrt); // LDR top right - BOTTOM RIGHT
-  int ml = analogRead(ldrml); // LDR mid left - TOP LEFT
-  int mr = analogRead(ldrrl); // LDR mid right - TOP RIGHT
-  int bl = analogRead(ldrbl); // LDR bottom left
-  int bm = analogRead(ldrbm); // LDR bottom mid
-  int tm = analogRead(ldrtm); // LDR top mid
-  int tr = analogRead(ldrtr); // LDR top right
-  int c = analogRead(ldrc); // LDR center - CENTER
+  // Read LDR values
+  int tl = analogRead(ldrTopLeft);     // Top left
+  int tm = analogRead(ldrTopMid);      // Top mid
+  int tr = analogRead(ldrTopRight);    // Top right
+  int ml = analogRead(ldrMidLeft);     // Mid left
+  int c = analogRead(ldrCenter);       // Center (Mid-Mid)
+  int mr = analogRead(ldrMidRight);    // Mid right
+  int bl = analogRead(ldrBottomLeft);  // Bottom left
+  int bm = analogRead(ldrBottomMid);   // Bottom mid
+  int br = analogRead(ldrBottomRight); // Bottom right
 
-  // Calculate average values
-  int avt = (lt + rt + tm + tr) / 4; // average value top
-  int avd = (bl + bm + ml + mr) / 4; // average value down
-  int avl = (lt + ml + bl) / 3; // average value left
-  int avr = (rt + mr + tr) / 3; // average value right
-  int avc = c; // average value center
+  int maxTop = max(tl, max(tm, tr));   // Maximum value from the top row
+  int maxBottom = max(bl, max(bm, br));// Maximum value from the bottom row
+  int maxLeft = max(tl, max(ml, bl));  // Maximum value from the left column
+  int maxRight = max(tr, max(mr, br)); // Maximum value from the right column
 
-  int dtime = 10;
-  int tol = 90;
+  int dtime = 10; // Delay time between adjustments
 
-  int dvert = avt - avd; // check the difference of up and down
-  int dhoriz = avl - avr; // check the difference of left and right
-
-
-  if (-1 * tol > dvert || dvert > tol) {
-    if (avt > avd) {
-      servov = ++servov;
-      if (servov > servovLimitHigh) {
-        servov = servovLimitHigh;
-      }
-    } else if (avt < avd) {
-      servov = --servov;
-      if (servov < servovLimitLow) {
-        servov = servovLimitLow;
-      }
+  // Vertical adjustment
+  if (maxTop > maxBottom) {
+    if (servov > servovLimitLow) {
+      servov--;
     }
-    vertical.write(servov);
-  }
-
-  if (-1 * tol > dhoriz || dhoriz > tol) {
-    if (avl > avr) {
-      servoh = --servoh;
-      if (servoh < servohLimitLow) {
-        servoh = servohLimitLow;
-      }
-    } else if (avl < avr) {
-      servoh = ++servoh;
-      if (servoh > servohLimitHigh) {
-        servoh = servohLimitHigh;
-      }
+  } else {
+    if (servov < servovLimitHigh) {
+      servov++;
     }
-    horizontal.write(servoh);
-      Serial.print( servoh);
-  Serial.print( servov);
   }
+  vertical.write(servov); // Move vertical servo
 
-  delay(dtime);
+  // Horizontal adjustment
+  if (maxLeft > maxRight) {
+    if (servoh > servohLimitLow) {
+      servoh--;
+    }
+  } else {
+    if (servoh < servohLimitHigh) {
+      servoh++;
+    }
+  }
+  horizontal.write(servoh); // Move horizontal servo
+
+  delay(dtime); // Small delay for stability
 }
